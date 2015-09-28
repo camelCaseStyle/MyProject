62be6f76d860084a17c79e4528e72728d938c62b
camelCaseStyle <anubhav.ashish9@gmail.com>
2015-09-28 23:31:10 +1000

    add hello.c

---
+++ hello.c
@@ -0,0 +1,17 @@
+/*
+This is a program 
+to just learn and mess 
+with git. Please do not take
+it seriously
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, const char * argv[]){
+	printf("Nothing is faster than speed of light, specially my typing speed.\n");
+	return 0;
+}
